1e971eeeddea785cccc43233fca784c41f6987c7
ekozat <emily.kozat@gmail.com>
2024-01-07 14:26:10 -0500

    add gameURL and function

--- src/valorant-api.js
+++ src/valorant-api.js
@@ -1,4 +1,5 @@
-const apiUrl = 'https://valorant-api.com/v1/agents';
+const agentUrl = 'https://valorant-api.com/v1/agents';
+const gameUrl = 'https://valorant-api.com/v1/gamemodes';
 // no API key needed - unofficial
 
 const headers = new Headers({
@@ -13,7 +14,30 @@ const requestOptions = {
 
 function fetchAgentData() {
     return new Promise((resolve, reject) => {
-        fetch(apiUrl, requestOptions)
+        fetch(agentUrl, requestOptions)
+            .then(response => {
+                if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                // first .then is conversion to json
+                return response.json();
+            })
+            .then(data => {
+                // resolve the promise
+                resolve(data);
+            })
+            .catch(error => {
+                // Handle errors
+                console.error('Error:', error);
+                reject(error);
+            });
+    });
+}
+
+function fetchGameData() {
+    return new Promise((resolve, reject) => {
+        fetch(gameUrl, requestOptions)
             .then(response => {
                 if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
@@ -37,4 +61,5 @@ function fetchAgentData() {
 // Export the fetchData function to make it accessible to other files
 module.exports = {
     fetchAgentData: fetchAgentData,
+    fetchGameData: fetchGameData,
 };
