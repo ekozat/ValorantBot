59e7217034d2b6bc476b96975d75f6bbbd936df3
ekozat <emily.kozat@gmail.com>
2024-01-07 19:20:43 -0500

    added weapon url

--- src/valorant-api.js
+++ src/valorant-api.js
@@ -1,5 +1,6 @@
 const agentUrl = 'https://valorant-api.com/v1/agents';
 const gameUrl = 'https://valorant-api.com/v1/gamemodes';
+const weaponUrl = 'https://valorant-api.com/v1/weapons';
 // no API key needed - unofficial
 
 const headers = new Headers({
@@ -58,8 +59,32 @@ function fetchGameData() {
     });
 }
 
+function fetchWeaponData() {
+    return new Promise((resolve, reject) => {
+        fetch(weaponUrl, requestOptions)
+            .then(response => {
+                if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                // first .then is conversion to json
+                return response.json();
+            })
+            .then(data => {
+                // resolve the promise
+                resolve(data);
+            })
+            .catch(error => {
+                // Handle errors
+                console.error('Error:', error);
+                reject(error);
+            });
+    });
+}
+
 // Export the fetchData function to make it accessible to other files
 module.exports = {
     fetchAgentData: fetchAgentData,
     fetchGameData: fetchGameData,
+    fetchWeaponData: fetchWeaponData,
 };
